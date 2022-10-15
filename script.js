54d02f9831dc2ba04ddfa598994279914544b450
Perrin Melwyn <melwyn_438@hotmail.com>
2022-10-15 09:23:40 +0200

    creation notice/resol beug js

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ function sonNewGame(){
   sonNewGame.src = "./son/nouvellePartie.mp3";
   sonNewGame.play();
 }
+function sonClique() {
+  let sonClique = document.createElement("audio");
+  sonClique.src = "./son/clique.wav";
+  sonClique.play();
+}
 
 function sonLancerDes (){
   let sonDes = document.createElement("audio");
@@ -23,6 +28,18 @@ function sonVictoire (){
   sonVictoire.play();
 }
 
+//Div regle du jeu masqué
+document.getElementById('regleDuJeu').style.visibility='hidden';
+//Ouvrir la regle du jeu
+function regleDuJeu (){
+  document.getElementById('regleDuJeu').style.visibility='visible';
+}
+//Fermer la regle du jeu
+function okey () {
+  document.getElementById('regleDuJeu').style.visibility='hidden';
+}
+
+
 function laMainJ1 (){
   jeux = 1;
   document.getElementById('laMainJ2').style.visibility='hidden';
@@ -33,78 +50,6 @@ function laMainJ2 (){
   jeux = 2;
   document.getElementById('laMainJ1').style.visibility='hidden';
   document.getElementById('laMainJ2').style.visibility='visible';
-}
-
-function joueur1 (){
-    laMainJ1();
-    if (lancer == 2) {
-    document.getElementById('imageDes').src = './des/dice-2.png';
-    sommeJ1 = sommeJ1 + lancer;
-    document.getElementById('scoreTempoJ1').innerHTML = sommeJ1;
-  }
-  else if (lancer == 3) {
-    document.getElementById('imageDes').src = './des/dice-3.png';
-    sommeJ1 = sommeJ1 + lancer;
-    document.getElementById('scoreTempoJ1').innerHTML = sommeJ1;
-  }
-  else if (lancer == 4) {
-    document.getElementById('imageDes').src = './des/dice-4.png';
-    sommeJ1 = sommeJ1 + lancer;
-    document.getElementById('scoreTempoJ1').innerHTML = sommeJ1;
-  }
-  else if (lancer == 5) {
-    document.getElementById('imageDes').src = './des/dice-5.png';
-    sommeJ1 = sommeJ1 + lancer;
-    document.getElementById('scoreTempoJ1').innerHTML = sommeJ1;
-  }
-  else if (lancer == 6) {
-    document.getElementById('imageDes').src = './des/dice-6.png';
-    sommeJ1 = sommeJ1 + lancer;
-    document.getElementById('scoreTempoJ1').innerHTML = sommeJ1;
-  }
-  else {
-    laMainJ2();
-    document.getElementById('imageDes').src = './des/dice-1.png';
-    sommeJ1 = 0;
-    document.getElementById('scoreTempoJ1').innerHTML = 0;
-    joueur2();
-  }
-}
-
-function joueur2 (){
-  laMainJ2();
-  if (lancer == 2) {
-    document.getElementById('imageDes').src = './des/dice-2.png';
-    sommeJ2 = sommeJ2 + lancer;
-    document.getElementById('scoreTempoJ2').innerHTML = sommeJ2;
-  }
-  else if (lancer == 3) {
-    document.getElementById('imageDes').src = './des/dice-3.png';
-    sommeJ2 = sommeJ2 + lancer;
-    document.getElementById('scoreTempoJ2').innerHTML = sommeJ2;
-  }
-  else if (lancer == 4) {
-    document.getElementById('imageDes').src = './des/dice-4.png';
-    sommeJ2 = sommeJ2 + lancer;
-    document.getElementById('scoreTempoJ2').innerHTML = sommeJ2;
-  }
-  else if (lancer == 5) {
-    document.getElementById('imageDes').src = './des/dice-5.png';
-    sommeJ2 = sommeJ2 + lancer;
-    document.getElementById('scoreTempoJ2').innerHTML = sommeJ2;
-  }
-  else if (lancer == 6) {
-    document.getElementById('imageDes').src = './des/dice-6.png';
-    sommeJ2 = sommeJ2 + lancer;
-    document.getElementById('scoreTempoJ2').innerHTML = sommeJ2;
-  }
-  else {
-    laMainJ1();
-    document.getElementById('imageDes').src = './des/dice-1.png';
-    sommeJ2 = 0;
-    document.getElementById('scoreTempoJ2').innerHTML = 0;
-    joueur1();
-  }
 }
 
 function nouvellePartie (){
@@ -125,41 +70,113 @@ function lancerDes (){
     sonLancerDes();
     lancer = Math.floor(Math.random() * 6) + 1;
     if (jeux == 1) {
-      joueur1();
+      switch(lancer){
+        case 1 :
+          sommeJ1 = 0;
+          document.getElementById('scoreTempoJ1').innerHTML = 0;
+          document.getElementById('imageDes').src = './des/dice-1.png'; 
+          laMainJ2();
+          sonClique();
+          break;
+        case 2 :
+          document.getElementById('imageDes').src = './des/dice-2.png';
+          sommeJ1 = sommeJ1 + lancer;
+          document.getElementById('scoreTempoJ1').innerHTML = sommeJ1;
+          break;
+        case 3 :
+          document.getElementById('imageDes').src = './des/dice-3.png';
+          sommeJ1 = sommeJ1 + lancer;
+          document.getElementById('scoreTempoJ1').innerHTML = sommeJ1;
+          break;
+        case 4 :
+          document.getElementById('imageDes').src = './des/dice-4.png';
+          sommeJ1 = sommeJ1 + lancer;
+          document.getElementById('scoreTempoJ1').innerHTML = sommeJ1;
+          break;
+        case 5 :
+          document.getElementById('imageDes').src = './des/dice-5.png';
+          sommeJ1 = sommeJ1 + lancer;
+          document.getElementById('scoreTempoJ1').innerHTML = sommeJ1;
+          break;
+        case 6 :
+          document.getElementById('imageDes').src = './des/dice-6.png';
+          sommeJ1 = sommeJ1 + lancer;
+          document.getElementById('scoreTempoJ1').innerHTML = sommeJ1;
+          break;
+        default:
+		      break;
+      }
     }
     else {
-      joueur2();
+      switch(lancer) {
+        case 1 :
+          sommeJ2 = 0;
+          document.getElementById('scoreTempoJ2').innerHTML = 0;
+          document.getElementById('imageDes').src = './des/dice-1.png'; 
+          laMainJ1();
+          sonClique();
+          break;
+        case 2 :
+          document.getElementById('imageDes').src = './des/dice-2.png';
+          sommeJ2 = sommeJ2 + lancer;
+          document.getElementById('scoreTempoJ2').innerHTML = sommeJ2;
+          break;
+        case 3 :
+          document.getElementById('imageDes').src = './des/dice-3.png';
+          sommeJ2 = sommeJ2 + lancer;
+          document.getElementById('scoreTempoJ2').innerHTML = sommeJ2;
+          break;
+        case 4 :
+          document.getElementById('imageDes').src = './des/dice-4.png';
+          sommeJ2 = sommeJ2 + lancer;
+          document.getElementById('scoreTempoJ2').innerHTML = sommeJ2;
+          break;
+        case 5 :
+          document.getElementById('imageDes').src = './des/dice-5.png';
+          sommeJ2 = sommeJ2 + lancer;
+          document.getElementById('scoreTempoJ2').innerHTML = sommeJ2;
+          break;
+        case 6 :
+          document.getElementById('imageDes').src = './des/dice-6.png';
+          sommeJ2 = sommeJ2 + lancer;
+          document.getElementById('scoreTempoJ2').innerHTML = sommeJ2;
+          break;
+        default:
+		      break;
+      }
     }
-}
+  }
 
 function hold (){
-  if(jeux == 1){
+  if((jeux == 1) && (sommeJ1 != 0)) {
     scoreTotalJ1 = sommeJ1 + scoreTotalJ1;
     document.getElementById('scoreTotalJ1').innerHTML = scoreTotalJ1;
     sommeJ1 = 0;
     document.getElementById('scoreTempoJ1').innerHTML = sommeJ1;
     laMainJ2 ();
+    sonClique();
     victoire ();
   }
   else {
+    if (sommeJ2 != 0) {
     scoreTotalJ2 = sommeJ2 + scoreTotalJ2;
     document.getElementById('scoreTotalJ2').innerHTML = scoreTotalJ2;
     sommeJ2 = 0;
     document.getElementById('scoreTempoJ2').innerHTML = sommeJ2;
     laMainJ1 ();
+    sonClique();
     victoire ();
+    }
   }
 }
 
 function victoire (){
   if (scoreTotalJ1 >= 100) {
     sonVictoire();
-    alert ('Le joueur 1 gagne avec un score total de ' + scoreTotalJ1 + ' points');
-    alert ('FIN DE LA PARTIE \nCLIQUER SUR "NEW GAME" POUR RECOMMENCER');
+    alert ('BRAVO \nLe joueur 1 gagne avec un score total de ' + scoreTotalJ1 + ' points \nFIN DE LA PARTIE \nCLIQUER SUR "NEW GAME" POUR RECOMMENCER');
   }
   else if (scoreTotalJ2 >= 100) {
     sonVictoire ();
-    alert ('Le joueur 2 gagne  avec un score total de ' + scoreTotalJ2 + ' points');
-    alert ('FIN DE LA PARTIE \nCLIQUER SUR "NEW GAME" POUR RECOMMENCER');
+    alert ('BRAVO \nLe joueur 2 gagne avec un score total de ' + scoreTotalJ1 + ' points \nFIN DE LA PARTIE \nCLIQUER SUR "NEW GAME" POUR RECOMMENCER');
   }
 }
