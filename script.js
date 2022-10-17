a90da70a3da685177b8d60044bcf352f338a6b6f
Perrin Melwyn <melwyn_438@hotmail.com>
2022-10-17 15:10:46 +0200

    modit affich

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//DECLARATION DES VARIABLES
 let jeux = 0;
 let lancer = 0;
 let sommeJ1 = 0;
@@ -5,6 +6,7 @@ let sommeJ2 = 0;
 let scoreTotalJ1
 let scoreTotalJ2 
 
+//SON / BRUITAGE
 function sonNewGame(){
   let sonNewGame = document.createElement("audio");
   sonNewGame.src = "./son/nouvellePartie.mp3";
@@ -15,44 +17,41 @@ function sonClique() {
   sonClique.src = "./son/clique.wav";
   sonClique.play();
 }
-
 function sonLancerDes (){
   let sonDes = document.createElement("audio");
   sonDes.src = "./son/dés.mp3";
   sonDes.play();
 }
-
 function sonVictoire (){
   let sonVictoire = document.createElement("audio");
   sonVictoire.src = "./son/victoire.mp3";
   sonVictoire.play();
 }
 
-//Div regle du jeu masqué
+//REGLE DU JEU MASQUE
 document.getElementById('regleDuJeu').style.visibility='hidden';
-//Ouvrir la regle du jeu
+
+//REGELE DU JEU VISIBLE
 function regleDuJeu (){
   document.getElementById('regleDuJeu').style.visibility='visible';
 }
-//Fermer la regle du jeu
+//FERMER LA REGLE DU JEU
 function okey () {
   document.getElementById('regleDuJeu').style.visibility='hidden';
 }
 
-
+//FONCTION D'AFFICHAGE J1 OU J2
 function laMainJ1 (){
   jeux = 1;
   document.getElementById('laMainJ2').style.visibility='hidden';
   document.getElementById('laMainJ1').style.visibility='visible';
 }
-
 function laMainJ2 (){
   jeux = 2;
   document.getElementById('laMainJ1').style.visibility='hidden';
   document.getElementById('laMainJ2').style.visibility='visible';
 }
-
-
+//FONCTION REMISE A ZERO DES SCORES EN FIN DE PARTIE
 function finDeLaPartie () {
   sommeJ1 = 0;
   sommeJ2 = 0;
@@ -63,7 +62,7 @@ function finDeLaPartie () {
   document.getElementById('scoreTotalJ1').innerHTML =  0;
   document.getElementById('scoreTotalJ2').innerHTML =  0;
 }
-
+//BOUTON NOUVELLE PARTIE
 function nouvellePartie (){
   sonNewGame();
   laMainJ1();
@@ -77,7 +76,7 @@ function nouvellePartie (){
   document.getElementById('scoreTotalJ2').innerHTML =  0;
   document.getElementById('imageDes').src = "./des/dice-6.png";
 }
-
+// BOUTON LANCER DES J1 OU J1 
 function lancerDes (){  
     sonLancerDes();
     lancer = Math.floor(Math.random() * 6) + 1;
@@ -158,7 +157,7 @@ function lancerDes (){
       }
     }
   }
-
+//BOUTON HOLD
 function hold (){
   if((jeux == 1) && (sommeJ1 != 0)) {
     scoreTotalJ1 = sommeJ1 + scoreTotalJ1;
@@ -181,7 +180,7 @@ function hold (){
     }
   }
 }
-
+//CONTROLE SI LE JOUEUR DEPASSE LES 100 POINTS
 function victoire (){
   if (scoreTotalJ1 >= 100) {
     sonVictoire();
